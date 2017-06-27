78c2a794b0c3fb41ce309d43136639ce6315944e
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-06-27 13:47:31 -0600

    refactors customer controller with valid search

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -1,19 +1,18 @@
 class Api::V1::CustomersController < ApplicationController
   def index
-    return render json: Customer.where(id: params["id"]) if params["id"]
-    return render json: Customer.where(first_name: params["first_name"]) if params["first_name"]
-    return render json: Customer.where(last_name: params["last_name"]) if params["last_name"]
-    return render json: Customer.where(created_at: params["created_at"].to_datetime.in_time_zone("UTC")) if params["created_at"]
-    return render json: Customer.where(updated_at: params["updated_at"].to_datetime.in_time_zone("UTC")) if params["updated_at"]
-    render json: Customer.all
+    return render json: Customer.all if valid_search.empty?
+    render json: Customer.where(valid_search)
   end
 
   def show
-    return render json: Customer.find_by(id: params["id"]) if params["id"]
-    return render json: Customer.find_by(first_name: params["first_name"]) if params["first_name"]
-    return render json: Customer.find_by(last_name: params["last_name"]) if params["last_name"]
-    return render json: Customer.find_by(created_at: params["created_at"].to_datetime.in_time_zone("UTC")) if params["created_at"]
-    return render json: Customer.find_by(updated_at: params["updated_at"].to_datetime.in_time_zone("UTC")) if params["updated_at"]
-    render json: Customer.find(params[:id])
+    render json: Customer.find_by(valid_search)
   end
+
+  private
+    def valid_search
+    date_search = {}
+    date_search[:created_at] = params["created_at"].to_datetime.in_time_zone("UTC") if params["created_at"]
+    date_search[:updated_at] = params["updated_at"].to_datetime.in_time_zone("UTC") if params["updated_at"]
+    params.permit(:id, :first_name, :last_name).merge(date_search)
+    end
 end
