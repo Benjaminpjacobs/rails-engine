fdcf5ce83dd6c124adf23b8b3da59dd6085ea49a
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-06-27 08:29:55 -0600

    creates route and endpoint for merchant invoices

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       namespace :merchants do
         get '/:id/items', to: 'items#index'
+        get '/:id/invoices', to: 'invoices#index'
       end
       
 
