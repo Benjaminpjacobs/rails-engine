30a2e1ef3f195e327d127d4f2028e93d844ea0f4
Alex Koch <alex549us3@gmail.com>
2017-06-26 21:17:08 -0600

    progress on routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :merchants, only: [:index]
+      namespace :merchants do
+        get '/find_all', to: 'find_merchants#index'
+      end
+      resources :merchants, only: [:index, :show]
+      resources :transactions, only: [:index, :show]
     end
   end
 end
--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -17,4 +17,65 @@ describe 'Merchants API' do
       expect(merchant["name"]).to be_a String
     end
   end
+
+  context "GET /api/v1/merchants/:id" do
+    it 'sends one merchant' do
+      merchant = create(:merchant)
+
+      get "/api/v1/merchants/#{merchant.id}"
+
+      expect(response).to be_success
+
+      raw_merchant = JSON.parse(response.body)
+
+      expect(raw_merchant["id"]).to eq(merchant.id)
+      expect(raw_merchant).to have_key("name")
+      expect(raw_merchant["name"]).to be_a String
+    end
+  end
+
+  context "GET /api/v1/merchants/find_all?params" do
+    it 'sends merchant(s) requested by id' do
+      merchant = create(:merchant)
+
+      id_params = { id: merchant.id }
+
+      get "/api/v1/merchants/find_all", params: id_params
+
+      expect(response).to be_success
+
+      raw_merchant = JSON.parse(response.body)
+
+      expect(raw_merchant["id"]).to eq(merchant.id)
+      expect(raw_merchant["name"]).to eq(merchant.name)
+    end
+
+    it 'sends merchant(s) requested by name' do
+      merchant = create(:merchant)
+
+      name_params = { name: merchant.name }
+      get "/api/v1/merchants/find_all", params: name_params
+
+      expect(response).to be_success
+
+      raw_merchant = JSON.parse(response.body)
+      expect(raw_merchant.count).to eq(1)
+
+      expect(raw_merchant.first["id"]).to eq(merchant.id)
+      expect(raw_merchant.first["name"]).to eq(merchant.name)
+    end
+
+    it 'sends merchant(s) requested by created_at' do
+      merchant = create(:merchant)
+
+      created_at_params = { created_at: merchant.created_at.to_s }
+
+      get '/api/v1/merchants/find_all', params: created_at_params
+
+      raw_merchant = JSON.parse(response.body)
+
+      expect(raw_merchant["id"]).to eq(merchant.id)
+      expect(raw_merchant["name"]).to eq(merchant.id)
+    end
+  end
 end
