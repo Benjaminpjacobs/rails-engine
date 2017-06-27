2e8b941001bbe65607e85d1fd3455bea389c98df
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-06-26 18:34:38 -0600

    creates routes and methods for invoice find all

--- app/controllers/api/v1/invoices_controller.rb
+++ app/controllers/api/v1/invoices_controller.rb
@@ -1,10 +1,18 @@
 class Api::V1::InvoicesController < ApplicationController
   def index
-    render json: Invoice.all
+    return render json: Invoice.where(customer_id: params[:customer_id].to_i) if params[:customer_id]
+    return render json: Invoice.where(merchant_id: params[:merchant_id].to_i) if params[:merchant_id]
+    return render json: Invoice.where(created_at: params[:created_at].to_datetime.in_time_zone("UTC")) if params[:created_at]
+    return render json: Invoice.where(updated_at: params[:updated_at].to_datetime.in_time_zone("UTC")) if params[:updated_at]
+    return render json: Invoice.all
   end
 
   def show
     return render json: Invoice.find_by(customer_id: params[:customer_id]) if params[:customer_id]
+    return render json: Invoice.find_by(merchant_id: params[:merchant_id]) if params[:merchant_id]
+    return render json: Invoice.find_by(status: params[:status]) if params[:status]
+    return render json: Invoice.find_by(created_at: params[:created_at].to_datetime.in_time_zone("UTC")) if params[:created_at]
+    return render json: Invoice.find_by(updated_at: params[:updated_at].to_datetime.in_time_zone("UTC")) if params[:updated_at]
     return render json: Invoice.find(params[:id])
   end
 end
