cb35a1e9b553383a4481abcf20ce2af9d79807a8
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-06-28 06:36:05 -0600

    adds valid_date method to clean up revenue controller

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -1,5 +1,12 @@
 class Api::V1::Merchants::RevenueController < ApplicationController
+  include DateSearch
   def index
-    return render json: Invoice.total_revenue(params[:date].to_datetime) if params[:date]
+    return render json: Invoice.total_revenue(valid_date) if valid_date
   end
+
+  private 
+
+    def valid_date
+      params[:date].to_datetime
+    end
 end
