7898306cef579336f95f86e6d35b27e70e05e606
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-06-29 10:21:13 -0600

    add swagger controllers to items sub controllers

--- app/controllers/api/v1/items/best_day_controller.rb
+++ app/controllers/api/v1/items/best_day_controller.rb
@@ -1,4 +1,13 @@
 class Api::V1::Items::BestDayController < ApplicationController
+  swagger_controller :items_best_day, "Items Best Day Controller"
+
+  swagger_api :show do
+    summary "Fetches the date of the item's best sale day"
+    param :query, :id, :integer, :optional, "id"
+
+    response :not_found
+  end
+
   def show
     render json: Item.find(params[:id]).best_day, serializer: BestDaySerializer
   end
