dfc7520623d9340aff6840478957a9ca64dcb39a
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-06-29 06:49:01 -0600

    generates swagger docs for main controllers

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -1,6 +1,35 @@
 class Api::V1::TransactionsController < ApplicationController
   include DateSearch
-  
+  include Swagger::Docs::Methods
+
+  swagger_controller :transactions, "Transactions"
+
+  swagger_api :index do
+    summary "Fetches all transactions"
+    notes "This lists all transactions, sortable by record attributes"
+    param :query, :find, :integer, :optional, "invoice_id"
+    param :query, :find, :string, :optional, "credit_card_number"
+    param :query, :find, :string, :optional, "result"
+    param :query, :find, :integer, :optional, "id"
+    param :query, :find, :datetime, :optional, "created_at"
+    param :query, :find, :datetime, :optional, "updated_at"
+    response :not_found
+    response :not_acceptable
+  end
+
+  swagger_api :show do
+    summary "Fetches single transaction"
+    notes "This returns a single transaction record by attributes"
+    param :path, :id, :integer, :required, "id"
+    param :query, :find, :integer, :optional, "invoice_id"
+    param :query, :find, :string, :optional, "credit_card_number"
+    param :query, :find, :string, :optional, "result"
+    param :query, :find, :datetime, :optional, "created_at"
+    param :query, :find, :datetime, :optional, "updated_at"
+    response :not_found
+    response :not_acceptable
+  end
+
   def index
     return render json: Transaction.all if valid_search.empty?
     render json: Transaction.where(valid_search)
