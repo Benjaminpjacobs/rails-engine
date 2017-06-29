da8e9928193275dc1599d327918bb003c0f65664
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-06-29 10:15:30 -0600

    adds swagger controllers to merchants and transactions controllers

--- app/controllers/api/v1/merchants/favorite_customer_controller.rb
+++ app/controllers/api/v1/merchants/favorite_customer_controller.rb
@@ -1,4 +1,16 @@
 class Api::V1::Merchants::FavoriteCustomerController < ApplicationController
+
+  include Swagger::Docs::Methods
+
+    swagger_controller :merchants_favorite, 'Merchants Favorite Customer Controller'
+
+  swagger_api :show do
+    summary "Fetches the favorite customer"
+    notes "This returns a single customer based on most successful transactions with given merchant"
+
+    param :path, :id, :integer, :required, "id"
+  end
+
   def show
     render json: Merchant.find(params[:id]).favorite_customer
   end 
--- app/controllers/api/v1/merchants/most_items_controller.rb
+++ app/controllers/api/v1/merchants/most_items_controller.rb
@@ -1,4 +1,16 @@
 class Api::V1::Merchants::MostItemsController < ApplicationController
+  include Swagger::Docs::Methods
+
+  swagger_controller :merchants_most_items, 'Merchants Most Items Controller'
+
+  swagger_api :index do
+    summary "Fetches merchants ranked by most items sold"
+    notes "This returns the merchants listed by total items sold, can be limited in quantity"
+    param :query, :date, :string, :optional, "quantity"
+  end
+
+
+
   def index
     return render json: Merchant.most_items(params[:quantity])
   end
